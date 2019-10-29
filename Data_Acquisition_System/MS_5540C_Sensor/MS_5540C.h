064ccb68d5646d56526782fc3882dfaf836fd8f1
AbdallhAbdelhamid <56974617+AbdallhAbdelhamid@users.noreply.github.com>
2019-10-29 22:15:37 +0200

    small modifications
    Moved calibration function into Preprocessor #calibrate

--- Data_Acquisition_System/MS_5540C_Sensor/MS_5540C.h
+++ Data_Acquisition_System/MS_5540C_Sensor/MS_5540C.h
@@ -8,15 +8,23 @@
 #ifndef MS_5540C_H_
 #define MS_5540C_H_
 
+
+/*If you are not in calibrate mode , Comment out next line  */
+
+#define CALIBRATE
+
+/************************************INCLUDES ***********************************/
+
 #include "common_macros.h"
 #include "micro_config.h"
 #include "std_types.h"
 #include "spi2.h"
 
+/******************************** Global Variables************************************/
 extern float32 g_Temp_Real; // Global variables contains the latest Temperature value
 extern float32 g_PCOMPHG; // Global variables contains the latest Pressure value
 
-
+/************************************ FUNCTIONS *****************************/
 void get_calb_parameters( void ); // calculates parameters and calibrations for sensor. initialize SPI before.
 void read_temp( void ); // Calculate TEMP and Pressure and puts values in global variables g_Temp_Real, g_PCOMPHG
 
