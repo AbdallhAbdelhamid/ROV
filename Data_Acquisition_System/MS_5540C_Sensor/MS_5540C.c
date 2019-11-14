2dd77012714e43045a4624d8ca95611e1d42410e
AbdallhAbdelhamid <56974617+AbdallhAbdelhamid@users.noreply.github.com>
2019-11-14 15:09:50 +0200

    Final version , tested
    Tested the Sensor , fixed some bugs and working.

--- Data_Acquisition_System/MS_5540C_Sensor/MS_5540C.c
+++ Data_Acquisition_System/MS_5540C_Sensor/MS_5540C.c
@@ -1,6 +1,7 @@
 /*
- * MS_5540C.c
- *	Module : MS 5540C Sensor .c
+ * 	MS_5540C.c
+ *	Module : MS 5540C Sensor Source File
+ *	Target : ATMEGA32
  *  Created on: Oct 24, 2019
  *  Author: Abdallh
  */
@@ -8,8 +9,44 @@
 #include "MS_5540C.h"
 
 // Spi_Config_Type spi_config = {F_CPU_64,FALLING,DOUBLE};
+ sint32 g_Temp_Real; // Global variables contains the latest Temperature value
+ sint32 g_PCOMPHG; // Global variables contains the latest Pressure value
 
-static uint16 C1,C2,C3,C4,C5,C6; // Paramaters to get from calibration words.
+#ifdef calibrate
+
+uint16 C1,C2,C3,C4,C5,C6; // Paramaters toget from calibration words.
+
+#endif
+
+#ifdef serial_debug
+
+char buffer3[12] = {0};		// only needed for debugging purposes.
+
+void print_serial_L(uint16 word)
+/**
+ * converts Long integers to strings and sends them via Serial interface
+ * input:
+ */
+{
+	UART_sendByte('\n');
+	ltoa(word,buffer3,10);	// long integer to string
+	UART_sendString(buffer3);
+}
+
+
+void print_serial_S(uint16 word)
+/**
+ * converts short integers to strings and sends them via Serial interface
+ * input:
+ */
+{
+	UART_sendByte('\n');
+	itoa(word,buffer3,10);	// short integer to string
+	UART_sendString(buffer3);
+}
+
+
+#endif
 
 void reset_sensor()
 {
@@ -22,46 +59,81 @@ void reset_sensor()
 
 #ifdef CALIBRATE
 
-void get_calb_parameters( void ) //calculates parameters and calibrations for sensor.
+void ms5540c_get_calb_parameters( void ) //calculates parameters and calibrations for sensor.
 {
-	uint16 word[4]=0;		// 4 variables for 4 calibration words.
+		UART_sendByte('\n');
+		UART_sendByte('S');
+
+	uint8 dummy=0;
+
+	uint16 word[4]={0,0,0,0};		// 4 variables for 4 calibration words.
+	spi_send_byte(0x00);				// get word 1
 	spi_send_byte(0x1D);				// get word 1
 	spi_send_byte(0x50);
-	word[0] = spi_recieve_byte();		// get Higher byte of word
+
+	word[0] = spi_recieve_byte(0x00);		// get Higher byte of word
+	dummy = spi_recieve_byte(0x00);
 	word[0] = word[0] << 8;				// shift left to put it in HIGH 8 bits
-	word[0] =( spi_recieve_byte() ) | ( word[0] );				// get lower byte of word
+	word[0] =( dummy |  word[0] );				// get lower byte of word
+
+
+	print_serial2_S(word[0]);
+
+
 	reset_sensor();
+
+
 
 	spi_send_byte(0x1D);				// get word 2
 	spi_send_byte(0x60);
-	word[1] = spi_recieve_byte();		// get Higher byte of word
+	/*Another method to get words*/
+//	word[1] = spi_recieve_byte(0x00);		// get Higher byte of word
+//	word[1] = word[1] << 8;				// shift left to put it in HIGH 8 bits
+//	word[1] =( spi_recieve_byte(0x00) ) | ( word[1] );		// get lower byte of word
+
+	word[1] = spi_recieve_byte(0x00);		// get Higher byte of word
+	dummy = spi_recieve_byte(0x00);
 	word[1] = word[1] << 8;				// shift left to put it in HIGH 8 bits
-	word[1] =( spi_recieve_byte() ) | ( word[1] );					// get lower byte of word
+	word[1] =( dummy |  word[1] );				// get lower byte of word
+
 	reset_sensor();
 
+	print_serial_S(word[1]);
 
 	spi_send_byte(0x1D);				// get word 3
 	spi_send_byte(0x90);
-	word[2] = spi_recieve_byte();		// get Higher byte of word
+	word[2] = spi_recieve_byte(0x00);		// get Higher byte of word
 	word[2] = word[2] << 8;				// shift left to put it in HIGH 8 bits
-	word[2] =( spi_recieve_byte() ) | ( word[2] );					// get lower byte of word
+	word[2] =( spi_recieve_byte(0x00) ) | ( word[2] );					// get lower byte of word
 	reset_sensor();
 
+	print_serial_S(word[2]);
+
+
 	spi_send_byte(0x1D);				// get word 4
-	spi_send_byte(0x60);
-	word[3] = spi_recieve_byte();		// get Higher byte of word
+	spi_send_byte(0xA0);
+	word[3] = spi_recieve_byte(0x00);		// get Higher byte of word
 	word[3] = word[3] << 8;				// shift left to put it in HIGH 8 bits
-	word[3] = 	word[3] =( spi_recieve_byte() ) | ( word[3] );		// get lower byte of word
+	word[3] = 	word[3] =( spi_recieve_byte(0x00) ) | ( word[3] );		// get lower byte of word
 	reset_sensor();
+
+	print_serial_S(word[3]);
 
 
 	// to get paramters
 	 C1 = (word[0]>>1 & 0x7FFF);											// Get C1
 	 C2 = ( ( (word[2] & 0x003F )<<6 ) | (word[3] & 0x003F) );			// Get C2
-	 C3 = ( (word[3] & 0xFFC0)>>6 );								// Get C3
-	 C4 = ( (word[2] & 0xFFC0)>>6 );								// Get C4
-	 C5 = ( ( word[1] & 0xFFC0)>>6 ) | ( (word[0] & 0x0001 ) << 10);	// get C5
+	 C3 = ((word[3] >> 6) & 0x03FF );								// Get C3
+	 C4 = ((word[2] >> 6) & 0x03FF );								// Get C4
+	 C5 = (((word[1	]>> 6) & 0x03FF)) | ( (word[0] & 0x0001 ) << 10);	// get C5
 	 C6 = (word[1] & 0x003F );
+
+	 print_serial_S(C1);
+	 print_serial_S(C2);
+	 print_serial_S(C3);
+	 print_serial_S(C4);
+	 print_serial_S(C5);
+	 print_serial_S(C6);
 
 }
 
@@ -73,9 +145,16 @@ static uint16 read_Pressure_raw()
 	spi_send_byte(0x0F); // send command of getting pressure ( MSB of address )
 	spi_send_byte(0x40); // send LSB of address
 	_delay_ms(35);		// wait for operation conversion complete
-	D1 = ( ( spi_recieve_byte() ) << 8 ); // Receive HIGH Byte of Pressure value
-	D1 = ( spi_recieve_byte() | D1 );   // Receive LOW Byte of D1 value and put it into LOW Of D1
+	D1 = ( ( spi_recieve_byte(0x00) ) << 8 ); // Receive HIGH Byte of Pressure value
+	D1 = ( spi_recieve_byte(0x00) | D1 );   // Receive LOW Byte of D1 value and put it into LOW Of D1
+#ifdef serial_debug
+		UART_send_byte('\n');
+		UART_sendString("PressureRaw");
+		print_serial2(D1);
+#endif
+
 	return D1;
+
 
 }
 
@@ -86,28 +165,41 @@ static uint16 read_temp_raw( void )
 	spi_send_byte(0x0F); // send command of getting temperature ( MSB of address )
 	spi_send_byte(0x20); // send LSB of address
 	_delay_ms(35);		// wait for operation conversion complete
-	D2 = ( ( spi_recieve_byte() ) << 8 ); // Receive HIGH Byte of temperature value
-	D2 = ( spi_recieve_byte() | D2 );   // Receive LOW Byte of D2 value and put it into LOW Of D2
+	D2 = ( ( spi_recieve_byte(0x00) ) << 8 ); // Receive HIGH Byte of temperature value
+	D2 = ( spi_recieve_byte(0x00) | D2 );   // Receive LOW Byte of D2 value and put it into LOW Of D2
+#ifdef serial_debug
+	UART_send_byte('\n');
+	UART_sendString("TempRaw\n");
+	print_serial2(D2);
+#endif
 	return D2;
 }
 
-void read_temp( void ) 		// calculation of real values
+
+void MS5540c_read_pressure_temp( void ) 		// calculation of real values
 {
 	uint16 D1 = read_Pressure_raw();
 	uint16 D2 = read_temp_raw();
 
-	const sint32 UT1 = (C5 << 3) + 20224;
-	const sint32 dT = D2 - UT1;
-	const sint32 TEMP = 200 + ((dT * (C6 + 50)) >> 10);
-	const sint32 OFF  = (C2 * 4) + (((C4 - 512) * dT) >> 12);
-	const sint32 SENS = C1 + ((C3 * dT) >> 10) + 24576;
-	const sint32 X = (SENS * (D1 - 7168) >> 14) - OFF;
-	long PCOMP = ((X * 10) >> 5) + 2500;
+	const long UT1 = (C5 << 3) + 20224;			// these calculations and equations are from MS5540c datasheet.
+	const long dT = D2 - UT1;
+	const long TEMP = 200 + ((dT * (C6 + 50)) >> 10);
+	const long OFF  = (C2 * 4) + (((C4 - 512) * dT) >> 12);
+	const long SENS = C1 + ((C3 * dT) >> 10) + 24576;
+	const long X = (SENS * (D1 - 7168) >> 14) - OFF;
+	const long PCOMP = ((X * 10) >> 5) + 2500;
 	g_Temp_Real = TEMP/10;
-	g_PCOMPHG = PCOMP * 750.06 / 10000; // mbar*10 -> mmHg === ((mbar/10)/1000)*750/06
-
-	// Code Upove needs some modifications in order to work in LOW and HIGH temperates ( T<20 & T > 45 )
-	long T2,P2;
+	g_PCOMPHG = PCOMP * 750.00 / 10000; // mbar*10 -> mmHg === ((mbar/10)/1000)*750.06
+#ifdef serial_debug
+	UART_send_byte('\n');
+	UART_sendString("PressureComb");
+	print_serial2(g_PCOMPHG);
+	UART_send_byte('\n');
+		UART_sendString("TempComb");
+		print_serial2(g_Temp_Real);
+#endif
+/*	// Code Upove needs some modifications in order to work in LOW and HIGH temperates ( T<20 & T > 45 )
+	long T2 = 0 ,P2 = 0;
 
 	if (TEMP < 200) 		// calculate for low temp < 20
 	    {
@@ -124,7 +216,10 @@ void read_temp( void ) 		// calculation of real values
 	  {
 	    g_Temp_Real = ( TEMP - T2 ) / 10 ;				// Degree
 	    g_PCOMPHG = (PCOMP - P2) * 750.06 / 10000;		// mbar*10 -> mmHg === ((mbar/10)/1000)*750/06
+
 	  }
+
+*/
 }
 
 
